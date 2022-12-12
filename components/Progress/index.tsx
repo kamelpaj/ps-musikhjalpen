a064e6f479842160b45de36a5c519f9640978661
kamelpaj <qurridutt@gmail.com>
2022-12-12 15:18:13 +0100

    fix deploy error

--- components/Progress/index.tsx
+++ components/Progress/index.tsx
@@ -1,10 +1,9 @@
-/* export default function Progress(props: {value: number}) {
-    return (
+export default function Progress(props: { value: number }) {
+  return "work in progress";
+  /* return (
         <div className=""="mb-1 text-lg font-medium dark:text-white">Large</div>
 <div className=""="mb-4 w-full h-4 bg-gray-200 rounded-full dark:bg-gray-700">
   <div className=""="h-4 bg-blue-600 rounded-full dark:bg-blue-500" style="width: 45%"></div>
 </div>
-    )
+    ) */
 }
-
- */
